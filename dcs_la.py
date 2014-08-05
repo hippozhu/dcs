3561613e4c67fcd25f21769d1218f65f62b8e19b
hippozhu <hippozhu@gmail.com>
2014-08-05 05:29:37 -0500

    8/4

--- dcs_la.py
+++ dcs_la.py
@@ -46,7 +46,12 @@ def overall_local_accuracy_proba(preds_neigh_proba, Y_neigh):
   acc = [map(lambda pred_neigh_proba: np.mean([x[y] for x, y in zip(pred_neigh_proba, y_neigh)]), pred_neigh_estimators_proba) for pred_neigh_estimators_proba, y_neigh in zip(preds_neigh_proba, Y_neigh)]
   return np.array(acc)
 
-#def local_class_accuracy():
+def local_class_accuracy(preds_neigh, YY_neigh, preds_test):
+  return np.vstack([class_accuracy(pred_neigh_estimators, y_neigh, pred_test) for pred_neigh_estimators, y_neigh, pred_test in zip(preds_neigh, YY_neigh, preds_test)])
+
+def class_accuracy(pred_neigh_estimators, y_neigh, pred_test):
+  cl_idx = map(lambda x: np.where(x[1]==x[0])[0], zip(pred_test, pred_neigh_estimators))
+  return [accuracy_score(y_neigh[idx], pred_neigh_estimators[i][idx]) if len(idx)>0 else .0 for i, idx in enumerate(cl_idx)]
   
 def dcs_la(X_train, y_train, X_test, y_test, clf, n_neigh):
   estimators = clf.estimators_
@@ -54,7 +59,7 @@ def dcs_la(X_train, y_train, X_test, y_test, clf, n_neigh):
   neigh.fit(X_train)
   preds_train = np.array(map(lambda e:e.predict(X_train), estimators))
   preds_train_proba = np.array(map(lambda e:e.predict_proba(X_train), estimators))
-  preds_test = np.array(map(lambda e:e.predict(X_test), estimators))
+  preds_test = np.array(map(lambda e:e.predict(X_test), estimators)).T
   preds_test_proba = np.array(map(lambda e:e.predict_proba(X_test), estimators))
   dist, knn = neigh.kneighbors(X_test)
   preds_neigh = np.array(map(lambda nn: preds_train[:,nn], knn))
@@ -66,45 +71,47 @@ def dcs_la(X_train, y_train, X_test, y_test, clf, n_neigh):
 
   ola = overall_local_accuracy(preds_neigh, YY_neigh)
   ola_proba = overall_local_accuracy_proba(preds_neigh_proba, YY_neigh)
+  lca = local_class_accuracy(preds_neigh, YY_neigh, preds_test)
   #best = ola.argmax(axis=1)
   #best_proba = ola_proba.argmax(axis=1)
   #pred_dcs = [clf.classes_[pred[idx]] for idx, pred in zip(best, preds_test.T)]
   #pred_dcs_proba = [clf.classes_[pred[idx]] for idx, pred in zip(best_proba, preds_test.T)]
-  pred_dcs = clf.classes_.take(map(lambda x: classify_by_ola(x[0], x[1]), zip(ola, preds_test.T)))
-  pred_dcs_proba = clf.classes_.take(map(lambda x: classify_by_ola_proba(x[0], x[1]), zip(ola_proba, preds_test.T)))
+  pred_dcs = clf.classes_.take(map(lambda x: classify_by_ola(x[0], x[1]), zip(ola, preds_test)))
+  pred_dcs_proba = clf.classes_.take(map(lambda x: classify_by_ola_proba(x[0], x[1]), zip(ola_proba, preds_test)))
   pred_dcs_nolast = clf.classes_.take(classify_no_last(ola, preds_test_proba.swapaxes(0,1), 30))
-  return pred_dcs, pred_dcs_proba, pred_dcs_nolast
+  pred_lca = clf.classes_.take(map(lambda x: classify_by_ola(x[0], x[1]), zip(lca, preds_test)))
+  pred_lca_nolast = clf.classes_.take(classify_no_last(lca, preds_test_proba.swapaxes(0,1), 30))
+  return pred_dcs, pred_dcs_proba, pred_dcs_nolast, pred_lca, pred_lca_nolast
 
 def dcs_cv(X, y, clf, n_neigh, p):
   acc_clf = []
   acc_dcs = []
   acc_dcs_proba = []
-  acc_dcs_new = []
+  acc_dcs_mid = []
   acc_dcs_nolast = []
+  acc_lca = []
+  acc_lca_nolast = []
   for train, test in StratifiedKFold(y, 3, random_state=random_state):
     X_train = X[train];y_train = y[train];X_test = X[test];y_test = y[test]
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    pred_dcs, pred_dcs_proba, pred_dcs_nolast = dcs_la(X_train, y_train, X_test, y_test, clf, n_neigh)
+    pred_dcs, pred_dcs_proba, pred_dcs_nolast, pred_lca, pred_lca_nolast = dcs_la(X_train, y_train, X_test, y_test, clf, n_neigh)
 
-    new_pred = y_pred.copy()
     preds_test = np.array(map(lambda e:e.predict(X_test), clf.estimators_))
     pred_avg = preds_test.mean(axis=0)
     mid = np.array(filter(lambda i: pred_avg[i]>=p and pred_avg[i]<=1-p, range(test.shape[0]))) 
-    #print mid.shape[0]
-    new_pred[mid] = pred_dcs[mid]
+    dcs_pred_mid = y_pred.copy()
+    dcs_pred_mid[mid] = pred_dcs[mid]
 
     acc_clf.append(accuracy_score(y_test, y_pred))
     acc_dcs.append(accuracy_score(y_test, pred_dcs))
     acc_dcs_proba.append(accuracy_score(y_test, pred_dcs_proba))
-    acc_dcs_new.append(accuracy_score(y_test, new_pred))
+    acc_dcs_mid.append(accuracy_score(y_test, new_pred))
     acc_dcs_nolast.append(accuracy_score(y_test, pred_dcs_nolast))
-  #print np.mean(acc_clf), acc_clf
-  #print np.mean(acc_dcs), acc_dcs
-  #print np.mean(acc_dcs_proba), acc_dcs_proba
-  #print np.mean(acc_dcs_new), acc_dcs_new
-  print '%.3f, %.3f, %.3f, %.3f, %.3f' %(np.mean(acc_clf), np.mean(acc_dcs), np.mean(acc_dcs_proba), np.mean(acc_dcs_new), np.mean(acc_dcs_nolast))
-  return np.mean(acc_clf), np.mean(acc_dcs), np.mean(acc_dcs_proba), np.mean(acc_dcs_new), np.mean(acc_dcs_nolast)
+    acc_lca.append(accuracy_score(y_test, pred_lca))
+    acc_lca_nolast.append(accuracy_score(y_test, pred_lca_nolast))
+  print '%.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f' %(np.mean(acc_clf), np.mean(acc_dcs), np.mean(acc_dcs_proba), np.mean(acc_dcs_mid), np.mean(acc_dcs_nolast), np.mean(acc_lca), np.mean(acc_lca_nolast))
+  return np.mean(acc_clf), np.mean(acc_dcs), np.mean(acc_dcs_proba), np.mean(acc_dcs_mid), np.mean(acc_dcs_nolast), np.mean(acc_lca), np.mean(acc_lca_nolast)
 
 '''
 def dcs(X, y):
@@ -124,7 +131,7 @@ if __name__ == '__main__':
   X, y = loadPima()
   #X, y = loadBreastCancer()
   #X, y = loadIonosphere()
-  #clf = BaggingClassifier(base_estimator=DecisionTreeClassifier(max_depth=depth), n_estimators=n_estimators)
-  clf = AdaBoostClassifier(n_estimators=n_estimators)
+  clf = BaggingClassifier(base_estimator=DecisionTreeClassifier(max_depth=depth), n_estimators=n_estimators)
+  #clf = AdaBoostClassifier(n_estimators=n_estimators)
   #clf = BaggingClassifier(n_estimators=n_estimators)
   print np.mean([dcs_cv(X, y, clf, n_neigh, p) for _ in xrange(10)], axis=0)
